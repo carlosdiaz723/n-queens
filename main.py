844d759eb69d0bfb02e1305e530cdf046c981ab4
Carlos A. Diaz <carlosdiaz723@yahoo.com>
2020-12-03 13:06:26 -0500

    added backtracking solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pprint import pprint as pretty
 
-n = 8
+n = 20
 isQueen = list()
 
 for i in range(n):
@@ -16,3 +16,44 @@ def flip(board, row, col):
         board[row][col] = not board[row][col]
     else:
         board[row][col] = True
+
+
+def printBoard(board):
+    for i in range(n):
+        for j in range(n):
+            if board[i][j]:
+                print('Q', end='\t')
+            else:
+                print('#', end='\t')
+        print()
+
+
+def isSafePlace(board, row, col):
+    for i in range(col):
+        if board[row][i]:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j]:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j]:
+            return False
+    return True
+
+
+def backtracking(board, col):
+    if col >= n:
+        return True
+
+    for i in range(n):
+        if isSafePlace(board, i, col):
+            board[i][col] = True
+            if backtracking(board, col + 1):
+                return True
+            else:
+                board[i][col] = False
+    return False
+
+
+if backtracking(isQueen, 0):
+    printBoard(isQueen)
